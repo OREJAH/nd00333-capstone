0bd95f908e795abaa887d8460864497310389d32
AYENI OREJAH <68875028+OREJAH@users.noreply.github.com>
2020-12-31 11:28:52 +0100

    Update score.py
    mkl

--- starter_file/score.py
+++ starter_file/score.py
@@ -2,24 +2,33 @@ import os
 import pickle
 import json
 import numpy
-from sklearn.externals import joblib
+import logging
+import joblib
 
 def init():
     global model
    # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # For multiple models, it points to the folder containing all deployed models (./azureml-models)
-    # model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'sklearn_classification_model.pkl')
-
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'sklearn_classification_model.pkl')
+    path = os.path.normpath(model_path)
+    path_split = path.split(os.sep)
+    log_server.update_custom_dimensions({'model_name': path_split[1], 'model_version': path_split[2]})
+    
+    try:
     # deserialize the model file back into a sklearn model
-    model = joblib.load(model_path)
-
+        model = joblib.load(model_path)
+    
+# Log the input and output data to appinsights:
+        info = {
+            "input": raw_data,
+            "output": result.tolist()
+            }
+        print(json.dumps(info))
 
 # note you can pass in multiple rows for scoring
 def run(raw_data):
     try:
-        data = json.loads(raw_data)['data']
-        data = numpy.array(data)
         result = model.predict(data)
         # you can return any datatype as long as it is JSON-serializable
         return result.tolist()
